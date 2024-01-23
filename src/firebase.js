478ec6715e48d1dbad37c7d38fbc9978eddb6eb8
A-Infogeek <rishabhraj026@gmail.com>
2024-01-23 19:51:05 +0530

    added env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {getDatabase} from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDNR0oQ4j2QqYHUUEmUmF14tv7rBSTXS6o",
-  authDomain: "task-management-99799.firebaseapp.com",
-  projectId: "task-management-99799",
-  storageBucket: "task-management-99799.appspot.com",
-  messagingSenderId: "120894439079",
-  appId: "1:120894439079:web:e363e14d12142155046582"
+  apiKey: process.env.REACT_APP_API_KEY || "",
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
